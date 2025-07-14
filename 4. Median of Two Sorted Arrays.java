9a04a468e9e1ca13dbb19f9813de87df377a0bf4
Sarveshwaran S <sarveshwaran1436@gmail.com>
2025-07-14 18:07:38 +0530

    Create 4. Median of Two Sorted Arrays.java

---
+++ 4. Median of Two Sorted Arrays.java
@@ -0,0 +1,16 @@
+public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] a=new int[nums1.length + nums2.length];
+        int index=0;
+        for(int i=0;i<nums1.length;i++){
+            a[index++]=nums1[i];
+        }
+        for(int i=0;i<nums2.length;i++){
+            a[index++]=nums2[i];
+        }
+        Arrays.sort(a);
+        int n=a.length;
+        if(n%2==0){
+            return (a[n/2-1]+a[n/2])/2.0;
+        }
+        return a[n/2];
+    }
